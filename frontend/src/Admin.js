b059cd85c1f3ece1b50b3e2568b207270df38ad7
Joseph Eiba <josepheiba@icloud.com>
2024-08-19 01:18:02 +0100

    fixing errors

---
+++ frontend/src/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './admin.css';
+
+function Admin() {
+  return (
+    <div>
+
+
+    </div>
+  );
+}
+
+export default Admin;
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Admin from "./Admin";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
