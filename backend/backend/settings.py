7ae876117743380abc247e1ebddcbd1ede3816b2
Joseph Eiba <josepheiba@icloud.com>
2025-05-30 22:22:26 +0100

    added allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scolivre.com', 'www.scolivre.com']
 
 
 # Application definition
